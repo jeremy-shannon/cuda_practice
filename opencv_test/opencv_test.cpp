d7289ad96a6393e49d425e7ae952bc1250465337
Jeremy Shannon <jeremyeshannon@gmail.com>
2024-08-20 13:27:17 -0400

    add simple test for openCV

---
+++ opencv_test/opencv_test.cpp
@@ -0,0 +1,35 @@
+
+#include <opencv2/opencv.hpp>
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+int main(int argc, char** argv)
+{
+    // Read the image file
+    Mat image = imread("../img.png");
+
+    // Check for failure
+    if (image.empty()) 
+    {
+        cout << "Could not open or find the image" << endl;
+        cin.get(); //wait for any key press
+        return -1;
+    }
+    else {
+        cout << "Image loaded!" << endl;    
+    }
+
+    //  String windowName = "The Guitar"; //Name of the window
+    //  namedWindow(windowName); // Create a window
+
+    imshow("window name", image); // (windowName, image); // Show our image inside the created window.
+
+    cout << "here" << endl;
+
+    waitKey(0); // Wait for any keystroke in the window
+    //  destroyWindow(windowName); //destroy the created window
+
+    return 0;
+}
